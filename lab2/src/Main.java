3a130435d81c067bf9afadf54bce8f9ce192e160
nmequalmn <meregodz@gmail.com>
2022-10-02 20:14:17 +0300

    Battle

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -4,10 +4,18 @@ import my.pokemons.*;
 public class Main {
     public static void main(String[] args) {
         Battle a = new Battle();
-        Pokemon k = new Pokemon("Typical", 1);
-        Pokemon b = new Scyther("Scyther", 1);
-        a.addFoe(k);
-        a.addAlly(b);
+        Pokemon Oleg = new Passimian("Passimian",1);
+        Pokemon Mark = new Scyther("Scyther",1);
+        Pokemon Kostya = new Scizor("Scizor",1);
+        Pokemon Michael = new Ralts("Ralts",1);
+        Pokemon Nikita = new Kirlia("Kirlia",1);
+        Pokemon Igor = new Gallade("Gallade",1);
+        a.addFoe(Oleg);
+        a.addFoe(Kostya);
+        a.addFoe(Nikita);
+        a.addAlly(Mark);
+        a.addAlly(Michael);
+        a.addAlly(Igor);
         a.go();
     }
 
