15797df2104e81eb0a012b26ea4fa78c33fa4bc1
nmequalmn <meregodz@gmail.com>
2022-10-02 20:07:00 +0300

    Gallade

--- lab2/src/my/pokemons/Gallade.java
+++ lab2/src/my/pokemons/Gallade.java
@@ -1,2 +1,15 @@
-package my.pokemons;public class Gallade {
+package my.pokemons;
+
+import my.pokemons.attacks.*;
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.Type;
+
+public class Gallade extends Pokemon {
+    public Gallade(String name, int level) {
+        super(name, level);
+        setStats(68,125,65,65,115,80);
+        setType(Type.PSYCHIC, Type.FIGHTING);
+        setMove(new Thunderbolt(), new ShadowBall(), new MagicalLeaf(), new Rest());
+        return;
+    }
 }
